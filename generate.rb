1448db286f6ea80963cee71b63c585702c9120c1
Katie Walsh <katie.katiewalsh@gmail.com>
2015-02-10 09:45:19 -0500

    My not-solution.

--- generate.rb
+++ generate.rb
@@ -2,10 +2,9 @@ require 'csv'
 
 File.open "commands", "w" do |f|
   CSV.foreach "new_users.csv" do |first_name, last_name, email|
-    # Generate the SQL to add this user to the database and write it to the file
-    f.puts command_to_add_user
+   f.puts "INSERT INTO new_users(first_name, last_name,email) VALUES (#{first_name}, #{last_name}, #{email});"
   end
 end
 
 puts "Wrote SQL queries to the file `commands`"
-puts "You can copy it to your pasteboard by typing `cat commands | pbcopy`
+puts "You can copy it to your pasteboard by typing `cat commands | pbcopy`"
