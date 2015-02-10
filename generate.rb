93f2438d26a0e48a91c44c2c489a39049a8d5415
Wonman Lee <wonmanlee@gmail.com>
2015-02-10 09:45:06 -0500

    My solution

--- generate.rb
+++ generate.rb
@@ -1,11 +1,14 @@
 require 'csv'
+# 50 users at start, select count(*) from users;
+
 
 File.open "commands", "w" do |f|
   CSV.foreach "new_users.csv" do |first_name, last_name, email|
+    command_to_add_user = "insert into users (first_name, last_name, email) values ('#{first_name}', '#{last_name}', '#{email}');"
     # Generate the SQL to add this user to the database and write it to the file
     f.puts command_to_add_user
   end
 end
 
 puts "Wrote SQL queries to the file `commands`"
-puts "You can copy it to your pasteboard by typing `cat commands | pbcopy`
+puts "You can copy it to your pasteboard by typing `cat commands | pbcopy`"
